d23ce13d0783467b2e0811b7a02197732b5318ef
stamatiap <s240146@dtu.dk>
2026-04-27 14:42:42 +0200

    Create TechniqueModel objects from the parsed description

--- src/translation/translator.py
+++ src/translation/translator.py
@@ -2,7 +2,9 @@ from antlr4 import FileStream, CommonTokenStream
 from src.cnl.grammar.CNLParser import CNLParser
 from src.cnl.grammar.CNLLexer import CNLLexer
 from src.translation.visitor import Visitor
-from src.domain_model.model import TechniqueModel
+from src.domain_model.model import *
+from src.domain_model.enums import *
+from src.domain_model.asset_registry import ASSET_TYPE_MAP
 import sys
 from pprint import pprint
 
@@ -18,6 +20,74 @@ class Translator:
         print(tree.toStringTree(recog=parser))
         return tree
 
+    def create_modifier(self, modifier) -> Modifier:
+        if modifier[0] == 'location':
+            mod_type = ModifierType.LOCATION
+        elif modifier[0] == 'destination':
+            mod_type = ModifierType.DESTINATION
+        elif modifier[0] == 'source_by':
+            mod_type = ModifierType.SOURCE_BY
+        elif modifier[0] == 'source_from':
+            mod_type = ModifierType.SOURCE_FROM
+        
+        return Modifier(
+            type = mod_type,
+            value = self.create_asset(modifier[1])
+        )
+
+    def create_asset(self, asset_name: str) -> Asset:
+        asset_type_str = self.assets.get(asset_name, None)
+        cls = ASSET_TYPE_MAP.get(asset_type_str.lower(), Asset)
+        return cls(type=asset_type_str, name=asset_name)
+
+    def create_state_conditions(self, conditions: list) -> list[StateCondition]:
+        return [self.create_state_condition(c) for c in conditions]
+
+    def create_state_condition(self, condition: dict) -> StateCondition:
+        subject = self.create_asset(condition["object"])
+        modifiers = [self.create_modifier(m) for m in condition.get("modifiers", [])]
+        return StateCondition(
+            subject=subject,
+            subject_state=condition["verb"],
+            modifiers=modifiers
+        )
+
+    def create_action(self, action) -> Action:
+        actor = self.create_asset(action.get('actor', None))
+        target = self.create_asset(action.get('object', None))
+        modifiers = [self.create_modifier(m) for m in action.get('modifiers', [])]
+        return Action(
+            action_verb = action.get('action_verb', None),
+            actor = actor,
+            target = target,
+            modifiers= modifiers
+        )
+
+    def create_event(self, event) -> Event:
+        action = self.create_action(event.get('when', {}))
+        preconditions = self.create_state_conditions(event.get('given', []))
+        postconditions = self.create_state_conditions(event.get('then', []))
+        return Event(
+            id = event.get('event_number', None),
+            name = "Event" + event.get('event_number', None),
+            action = action,
+            preconditions = preconditions,
+            operator = LogicalOperatorType.AND,
+            postconditions = postconditions
+        )
+
+    def create_technique_model(self, parsed_data) -> TechniqueModel:
+        # background + detection
+        self.assets = parsed_data.get('assets', {})
+        event_objects = [self.create_event(event) for event in parsed_data.get('events', [])]
+        return TechniqueModel(
+            id=parsed_data.get("technique_id", ""),
+            name=parsed_data.get("technique_name", ""),
+            tactic=parsed_data.get("tactic_name", ""),
+            tactic_id=parsed_data.get("tactic_id", ""),
+            events= event_objects
+        )
+    
     def translate(self, cnl_input_path: str) -> TechniqueModel:
         # Parse the CNL input using the grammar
         parsed_data = self.parse_input(cnl_input_path)
@@ -26,18 +96,8 @@ class Translator:
         pprint(raw_strings)
 
         # Convert the parsed data into a TechniqueModel
-        technique_model = self.convert_to_technique_model(raw_strings)
+        technique_model = self.create_technique_model(raw_strings)
         return technique_model
-
-    def convert_to_technique_model(self, parsed_data) -> TechniqueModel:
-        # This is a placeholder implementation. The actual conversion logic will depend on the structure of parsed_data.
-        return TechniqueModel(
-            id=parsed_data.get('id', 'default_id'),
-            name=parsed_data.get('name', 'default_name'),
-            tactic=parsed_data.get('tactic', 'default_tactic'),
-            tactic_id=parsed_data.get('tactic_id', 'default_tactic_id'),
-            events=parsed_data.get('events', [])
-        )
     
 def main():
     if len(sys.argv) < 2:
