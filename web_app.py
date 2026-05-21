87ab428174c3975b513763d8bd2c02174c441f3d
stamatiap <s240146@dtu.dk>
2026-05-21 10:20:27 +0200

    Add first experiment of creating an interactive web app for the pipeline

---
+++ web_app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from src.translation.translator import Translator
+from src.petri_net.petri_net_builder import PetriNetBuilder
+
+st.write("Let's get to modeling!")
+# get all example
+technique_title = st.text_input("Which technique are you modeling?")
+if technique_title:
+    file_path = f"streamlit_testing_{technique_title.replace(" " ,"_")}"
+
+    description_text = st.text_area("Please write a MITRE ATT&CK technique in CNL:", height="content")
+
+    if description_text:
+        path = "data/example_descriptions/"+ file_path + ".txt"
+        with open(path, "w+") as text_file:
+            text_file.write(description_text)
+
+            
+        translator = Translator()
+        model = translator.translate(path)
+        petri_net_builder = PetriNetBuilder()
+        petri_net_builder.visualize(*petri_net_builder.build(model), "testing_streamlit")
+
+        st.image("testing_streamlit.png", caption=f"Petri Net for {path.split('/')[-1][:-4]}")
